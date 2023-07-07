78bc92e5cfbc9f119a3e167514cdcbd80f72f429
unknown <skltfldl165@gmail.com>
2023-07-07 13:54:05 +0800

    一个类似于原始timesnet的dataset

---
+++ dataprovider.py
@@ -0,0 +1,59 @@
+from torch.utils.data import Dataset
+import pandas as pd
+
+
+class Dataset_My(Dataset):
+    def __init__(self, path=r".\data\ETTm1.csv", size=(384, 96), target="OT", flag="train"):
+        super(Dataset_My, self).__init__()
+        
+        self.seq = size[0]
+        self.pred = size[1]
+        
+        if flag == "train":
+            self.stage = 0
+        elif flag == "val":
+            self.stage = 1
+        else:
+            self.stage = 2
+
+        board1s = [0, 360 * 96 - self.seq, 480 * 96 - self.seq]
+        board2s = [360 * 96, 480 * 96, 600 * 96]
+        board1 = board1s[self.stage]
+        board2 = board2s[self.stage]
+
+        data_pd = pd.read_csv(path)
+        data = data_pd[data_pd.columns[1:]]
+        self.data = data[board1:board2]
+        
+        stamp = data_pd[['date']][board1:board2]
+        stamp['date'] = stamp['date'].map(pd.to_datetime)
+        stamp['month'] = stamp['date'].map(lambda x:x.month)
+        stamp['weekday'] = stamp['date'].map(lambda x:x.weekday())
+        stamp['day'] = stamp['date'].map(lambda x:x.day)
+        stamp['hour'] = stamp['date'].map(lambda x:x.hour)
+        stamp['minute'] = stamp['date'].map(lambda x:x.minute // 15)
+        stamp = stamp.drop(labels='date', axis=1)
+        print(stamp.shape)
+        self.stamp = stamp
+
+
+    def __getitem__(self, index):
+        sx = index
+        ex = index + self.seq
+        sy = ex
+        ey = sy + self.pred
+        x = self.data[sx:ex]
+        y = self.data[sy:ey]
+        xm = self.stamp[sx:ex]
+        ym = self.stamp[sy:ey]
+        return x, xm, y, ym
+
+
+    def __len__(self):
+        return len(self.data) - self.pred - self.seq + 1
+
+if __name__ == "__main__":
+    dataset = Dataset_My()
+    for i in range(len(dataset)):
+        x = dataset[i]
+        print(i, x[0].shape, x[1].shape, x[2].shape, x[3].shape)
