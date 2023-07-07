cfcc033f4c2d65ab3a421b3fe1dcbd9a328dde2f
unknown <skltfldl165@gmail.com>
2023-07-07 16:35:48 +0800

    main里面实现了加载train和validation数据，基本框架已经搭好了

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from dataprovider import Dataset_My
 from timesnetmodel import Timesnet
 from torch.utils.data import DataLoader
+from pytorch_lightning.trainer import Trainer
 
+train_dataset = Dataset_My(flag="train")
+val_dataset = Dataset_My(flag="val")
+train_dataloader = DataLoader(batch_size=128, shuffle=False, dataset=train_dataset)
+val_dataloader = DataLoader(batch_size=128, shuffle=False, dataset=val_dataset)
 
-dataset = Dataset_My()
-dataloader = DataLoader(batch_size=128, shuffle=False, dataset=dataset)
+model = Timesnet()
 
-for x, y, z, w in dataloader:
-    print(x.shape, y.shape, z.shape, w.shape)
+trainer = Trainer()
+
+# trainer.fit(model=model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
