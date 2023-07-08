79b222c852eace622d1ffadc4e2b68c92b64b069
unknown <skltfldl165@gmail.com>
2023-07-08 13:51:43 +0800

    trainingstep一般需要返回一个loss就可以了，这里就另起一个nn.model来进行forward

--- timesnetmodel.py
+++ timesnetmodel.py
@@ -1,13 +1,32 @@
 import pytorch_lightning as pl
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Block():
+    def __init__(self):
+        pass
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+
+    
+    def forward(self, x, x_mark):
+        pass
 
 
 class Timesnet(pl.LightningModule):
     def __init__(self):
         super(Timesnet, self).__init__()
+        self.model = Model()
 
 
     def training_step(self, batch, batch_idx):
         x, x_mask, y, y_mask = batch
+        y_hat = self.model(x, x_mask)
+        return F.mse_loss(y_hat, y)
 
 
     def validation_step(self):
@@ -16,3 +35,7 @@ class Timesnet(pl.LightningModule):
 
     def configure_optimizers(self):
         pass
+
+
+
+
